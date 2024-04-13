c56e1a31616bf45c382d5414261b7c15ac1f7890
OmarFathy22 <of405789@gmail.com>
2024-04-13 07:32:14 +0200

    course controller finished

--- middleware/errors/handleError.js
+++ middleware/errors/handleError.js
@@ -1,10 +1,8 @@
 const errorHandler = (err, req, res, next) => {
-  console.log(err);
-
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
 
-  return res.status(statusCode).json({ message:message });
+  return res.status(statusCode).json({ message: message });
 };
 
 export default errorHandler;
