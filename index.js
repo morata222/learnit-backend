dc4725a746de0610410c14cb252c0c372927490e
morata222 <of2222002@gmai.com>
2024-05-02 14:40:40 +0300

    update index route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // import packages
 import express from "express";
 import bodyParser from "body-parser";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 import MongoConnection from "./config/db.js";
 import errorHandler from "./middleware/errors/handleError.js";
 import Routes from "./routes/index.js";
-import {scheduleMonthlyRankingsReset} from './utils/checkStartOfMonth.js'
+import { scheduleMonthlyRankingsReset } from "./utils/checkStartOfMonth.js";
 
 // middlewares
 const app = express(); // express app
@@ -17,9 +17,11 @@ dotenv.config(); // environment variables
 app.use(cors()); // cors
 app.use(bodyParser.json()); // parse application/json
 app.use(cookieParser()); // parse cookies
-app.use('/' , Routes); // Using the routes
+app.use("/", Routes); // Using the routes
 app.use(errorHandler); // Using the custom error handling middleware should be the last middleware in the stack
-app.get("/" , (req , res) => { console.log("app is running");})
+app.get("/", (req, res) => {
+  res.send("Welcome to the backend of the e-learning platform");
+});
 
 scheduleMonthlyRankingsReset(); // Schedule the monthly rankings reset
 
