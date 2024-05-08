42db686f42df9c5f7ef823b96c5947cc2d50f057
morata222 <of2222002@gmail.com>
2024-05-09 01:17:48 +0300

    testing

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // import packages
 import express from "express";
 import bodyParser from "body-parser";
-<<<<<<< HEAD
 import cookieParser from "cookie-parser";
-=======
-import cookieParser from 'cookie-parser';
->>>>>>> 5569416dc245e602c972125efd4524508195ac6d
 import cors from "cors";
 import dotenv from "dotenv";
 
@@ -13,11 +9,7 @@ import dotenv from "dotenv";
 import MongoConnection from "./config/db.js";
 import errorHandler from "./middleware/errors/handleError.js";
 import Routes from "./routes/index.js";
-<<<<<<< HEAD
 import { scheduleMonthlyRankingsReset } from "./utils/checkStartOfMonth.js";
-=======
-import {scheduleMonthlyRankingsReset} from './utils/checkStartOfMonth.js'
->>>>>>> 5569416dc245e602c972125efd4524508195ac6d
 
 // middlewares
 const app = express(); // express app
@@ -25,11 +17,7 @@ dotenv.config(); // environment variables
 app.use(cors()); // cors
 app.use(bodyParser.json()); // parse application/json
 app.use(cookieParser()); // parse cookies
-<<<<<<< HEAD
 app.use("/", Routes); // Using the routes
-=======
-app.use('/' , Routes); // Using the routes
->>>>>>> 5569416dc245e602c972125efd4524508195ac6d
 app.use(errorHandler); // Using the custom error handling middleware should be the last middleware in the stack
 app.get("/", (req, res) => {
   res.send("Welcome to the backend of the e-learning platform");
