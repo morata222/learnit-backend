2af828bc3cc51a093c150ed317a9f18213585e86
OmarFathy22 <of405789@gmail.com>
2024-06-25 14:23:22 +0300

    update

--- controllers/user/leaderboard.js
+++ controllers/user/leaderboard.js
@@ -2,18 +2,22 @@ import UserProgress from "../../models/user/user-progress.js";
 
 export const getAllUsers = async (req, res, next) => {
   try {
-    const users = await UserProgress.find().populate("userID", "username email photoUrl");
+    const users = await UserProgress.find()
+      .populate("userID", "username email photoUrl")
+      .sort({ points: -1 }); // Sort users by points in descending order
     res.status(200).json(users);
   } catch (error) {
     next(error);
   }
-}
+};
+
 export const getMonthlyRanking = async (req, res, next) => {
   try {
-    const users = await UserProgress.find().populate("userID", "username email photoUrl");
+    const users = await UserProgress.find()
+      .populate("userID", "username email photoUrl")
+      .sort({ points: -1 }); // Sort users by points in descending order
     res.status(200).json(users);
   } catch (error) {
     next(error);
   }
-}
-
+};
